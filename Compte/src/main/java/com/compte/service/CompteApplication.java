4abe23bb2016282f4838b123cf2a370c52f875de
achaghour zakaria <achaghourzakaria@gmail.com>
2023-10-24 16:21:59 +0100

    start

---
+++ Compte/src/main/java/com/compte/service/CompteApplication.java
@@ -0,0 +1,13 @@
+package com.compte.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteApplication.class, args);
+    }
+
+}
---
+++ Compte/src/test/java/com/compte/service/CompteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compte.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
